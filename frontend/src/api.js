a8f7bfb0f1d9edf5207820cb8f943b08754cdf9f
Deepika-Kondapalli3 <kondapallideepika03@gmail.com>
2025-09-21 12:44:27 +0530

    changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
  import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/', // proxy in package.json routes to backend
+   baseURL: process.env.REACT_APP_API_URL || '/', // proxy in package.json routes to backend
   headers: {
     'Content-Type': 'application/json'
   },
